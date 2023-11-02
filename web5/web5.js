d4822ab6b6b7609f045e458605136e37b690a871
Liprinka <144840720+Liprinka@users.noreply.github.com>
2023-11-02 07:41:58 +0300

    Add files via upload

---
+++ web5/web5.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded and parsed");
+
+    function onClick(e) {
+        const price = [5, 10, 50];
+        const item = document.getElementById('select');
+        const count = document.getElementById('Count');
+        let result = document.getElementById('result');
+        if (/^\d+$/.test(count.value)) {
+            result.value = price[item.value] * count.value + '$';
+        } else {
+            alert('для ввода доступны только числа');
+        }
+        e.preventDefault();
+    }
+
+    const btn = document.getElementById('button');
+    btn.addEventListener('click', onClick);
+});
